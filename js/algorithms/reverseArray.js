c7532174ef88ce51e17f237cda952500ab54423e
Emmanuel Madeyi <emma.madeyi.nuetion.com>
2020-04-23 20:30:36 +0100

    new algorithms add

--- js/algorithms/reverseArray.js
+++ js/algorithms/reverseArray.js
@@ -1,12 +1,11 @@
 function reverseArrayInPlace(arr) {
     for (let i = 0; i < arr.length / 2; i++) {
         let tempVal = arr[i];
-
-        console.log(tempVal, arr.length, arr.length - 1, arr.length - 1 - i, i);
+        // console.log(tempVal, arr.length, arr.length - 1, arr.length - 1 - i, i);
         arr[i] = arr[arr.length - 1 - i];
-        console.log(i, arr[i]);
+        // console.log(i, arr[i]);
         arr[arr.length - 1 - i] = tempVal;
-        console.log(i, arr[arr.length - 1 - i]);
+        // console.log(arr.length - 1 - i, arr[arr.length - 1 - i]);
     }
     return arr;
 }
